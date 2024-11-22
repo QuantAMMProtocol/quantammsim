6d8d4f21217181d4ef27dab613f9e7b1f861bb55
Matthew Willetts <matthew@Matthews-MacBook-Air-2.local>
2024-11-22 09:40:30 +0000

    add hooks to simulator (both dynamic fee hooks and also hooks for transforming pools to calculate RVR/LVR as their reserve calculations)

--- quantammsim/pools/FM_AMM/cow_pool_8020.py
+++ quantammsim/pools/FM_AMM/cow_pool_8020.py
@@ -26,15 +26,14 @@ from functools import partial
 import numpy as np
 
 from quantammsim.pools.base_pool import AbstractPool
-from quantammsim.pools.G3M.balancer.balancer_reserves import (
-    _jax_calc_balancer_reserve_ratios,
-    _jax_calc_balancer_reserves_with_fees_using_precalcs,
-    _jax_calc_balancer_reserves_with_dynamic_inputs,
+from quantammsim.pools.FM_AMM.cow_reserves import (
+    _jax_calc_cowamm_reserve_ratio_vmapped,
+    _jax_calc_cowamm_reserves_with_weights_with_fees,
 )
 from quantammsim.core_simulator.param_utils import make_vmap_in_axes_dict
 
 
-class BalancerPool(AbstractPool):
+class CowPool8020(AbstractPool):
     def __init__(self):
         super().__init__()
 
@@ -47,7 +46,7 @@ class BalancerPool(AbstractPool):
         start_index: jnp.ndarray,
         additional_oracle_input: Optional[jnp.ndarray] = None,
     ) -> jnp.ndarray:
-        weights = self.calculate_weights(params)
+        weights = self.calculate_weights()
         bout_length = run_fingerprint["bout_length"]
         n_assets = run_fingerprint["n_assets"]
         local_prices = dynamic_slice(prices, start_index, (bout_length - 1, n_assets))
@@ -62,14 +61,13 @@ class BalancerPool(AbstractPool):
         initial_value_per_token = weights * initial_pool_value
         initial_reserves = initial_value_per_token / local_prices[0]
 
-        reserves = _jax_calc_balancer_reserves_with_fees_using_precalcs(
+        reserves = _jax_calc_cowamm_reserves_with_weights_with_fees(
             initial_reserves,
             weights,
             arb_acted_upon_local_prices,
             fees=run_fingerprint["fees"],
             arb_thresh=run_fingerprint["gas_cost"],
             arb_fees=run_fingerprint["arb_fees"],
-            all_sig_variations=jnp.array(run_fingerprint["all_sig_variations"]),
         )
         return reserves
 
@@ -82,7 +80,7 @@ class BalancerPool(AbstractPool):
         start_index: jnp.ndarray,
         additional_oracle_input: Optional[jnp.ndarray] = None,
     ) -> jnp.ndarray:
-        weights = self.calculate_weights(params)
+        weights = self.calculate_weights()
         bout_length = run_fingerprint["bout_length"]
         n_assets = run_fingerprint["n_assets"]
         local_prices = dynamic_slice(prices, start_index, (bout_length - 1, n_assets))
@@ -96,21 +94,15 @@ class BalancerPool(AbstractPool):
 
         # calculate initial reserves
         initial_pool_value = run_fingerprint["initial_pool_value"]
-        initial_value_per_token = weights * initial_pool_value
+        initial_value_per_token = jnp.array([0.5, 0.5]) * initial_pool_value
         initial_reserves = initial_value_per_token / local_prices[0]
 
-        reserve_ratios = _jax_calc_balancer_reserve_ratios(
-            arb_acted_upon_local_prices[:-1],
+        reserves = _jax_calc_cowamm_reserves_with_weights_with_fees(
+            initial_reserves,
+            arb_acted_upon_local_prices,
             weights,
-            arb_acted_upon_local_prices[1:],
-        )
-
-        # calculate the reserves by cumprod of reserve ratios
-        reserves = jnp.vstack(
-            [
-                initial_reserves,
-                initial_reserves * jnp.cumprod(reserve_ratios, axis=0),
-            ]
+            arb_thresh=run_fingerprint["gas_cost"],
+            arb_fees=run_fingerprint["arb_fees"],
         )
         return reserves
 
@@ -127,49 +119,7 @@ class BalancerPool(AbstractPool):
         trade_array: jnp.ndarray,
         additional_oracle_input: Optional[jnp.ndarray] = None,
     ) -> jnp.ndarray:
-        bout_length = run_fingerprint["bout_length"]
-        n_assets = run_fingerprint["n_assets"]
-
-        local_prices = dynamic_slice(prices, start_index, (bout_length - 1, n_assets))
-        weights = self.calculate_weights(params)
-
-        if run_fingerprint["arb_frequency"] != 1:
-            arb_acted_upon_local_prices = local_prices[:: run_fingerprint["arb_frequency"]]
-        else:
-            arb_acted_upon_local_prices = local_prices
-
-        initial_pool_value = run_fingerprint["initial_pool_value"]
-        initial_value_per_token = weights * initial_pool_value
-        initial_reserves = initial_value_per_token / arb_acted_upon_local_prices[0]
-
-        # any of fees_array, arb_thresh_array, arb_fees_array, trade_array
-        # can be singletons, in which case we repeat them for the length of the bout
-
-        # Determine the maximum leading dimension
-        max_len = bout_length - 1
-        if run_fingerprint["arb_frequency"] != 1:
-            max_len = max_len // run_fingerprint["arb_frequency"]
-        # Broadcast input arrays to match the maximum leading dimension.
-        # If they are singletons, this will just repeat them for the length of the bout.
-        # If they are arrays of length bout_length, this will cause no change.
-        fees_array_broadcast = jnp.broadcast_to(fees_array, (max_len,) + fees_array.shape[1:])
-        arb_thresh_array_broadcast = jnp.broadcast_to(arb_thresh_array, (max_len,) + arb_thresh_array.shape[1:])
-        arb_fees_array_broadcast = jnp.broadcast_to(arb_fees_array, (max_len,) + arb_fees_array.shape[1:])
-        # if we are doing trades, the trades array must be of the same length as the other arrays
-        if run_fingerprint["do_trades"]:
-            assert trade_array.shape[0] == max_len
-        reserves = _jax_calc_balancer_reserves_with_dynamic_inputs(
-            initial_reserves,
-            weights,
-            arb_acted_upon_local_prices,
-            fees_array_broadcast,
-            arb_thresh_array_broadcast,
-            arb_fees_array_broadcast,
-            jnp.array(run_fingerprint["all_sig_variations"]),
-            trade_array,
-            run_fingerprint["do_trades"],
-        )
-        return reserves
+        raise NotImplementedError("CowPoolWeights does not yet implement calculate_reserves_with_dynamic_inputs")
 
     def _init_base_parameters(
         self,
@@ -216,14 +166,8 @@ class BalancerPool(AbstractPool):
         params = self.add_noise(params, noise, n_parameter_sets)
         return params
 
-    def calculate_weights(
-        self, params: Dict[str, jnp.ndarray], *args, **kwargs
-    ) -> jnp.ndarray:
-        initial_weights_logits = params.get("initial_weights_logits")
-        # we dont't want to change the weights during any training
-        # so wrap them in a stop_grad
-        weights = softmax(stop_gradient(initial_weights_logits))
-        return weights
+    def calculate_weights(self) -> jnp.ndarray:
+        return jnp.array([0.8, 0.2])
 
     def make_vmap_in_axes(self, params: Dict[str, Any], n_repeats_of_recurred: int = 0):
         return make_vmap_in_axes_dict(params, 0, [], [], n_repeats_of_recurred)
@@ -233,7 +177,7 @@ class BalancerPool(AbstractPool):
 
 
 tree_util.register_pytree_node(
-    BalancerPool,
-    BalancerPool._tree_flatten,
-    BalancerPool._tree_unflatten,
+    CowPool8020,
+    CowPool8020._tree_flatten,
+    CowPool8020._tree_unflatten,
 )
