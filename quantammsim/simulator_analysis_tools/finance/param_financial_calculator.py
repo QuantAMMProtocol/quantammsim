47a0b754268035e460ca743804bb83f331d1f807
christian harrington <christian@bulkbit.systems>
2025-01-21 18:59:33 +0000

    fix

--- quantammsim/simulator_analysis_tools/finance/param_financial_calculator.py
+++ quantammsim/simulator_analysis_tools/finance/param_financial_calculator.py
@@ -168,11 +168,12 @@ def run_pool_simulation(simulationRunDto):
         "do_trades": raw_trades is not None,
         "return_val": "final_reserves_value_and_weights",
         "do_arb": simulationRunDto.pool.enableAutomaticArbBots,
-        "numeraire":simulationRunDto.pool.numeraire,
+        "numeraire":simulationRunDto.pool.poolNumeraireCoinCode,
     }
 
     price_data_local = get_historic_parquet_data(tokens)
-
+    print(run_fingerprint)
+    print(update_rule_parameter_dict_converted)
     outputDict = do_run_on_historic_data(
         run_fingerprint,
         update_rule_parameter_dict_converted,
